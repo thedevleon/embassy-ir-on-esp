17309281e97010b429f84192dbaf973a44214225
Leon <12817042+thedevleon@users.noreply.github.com>
2024-03-16 16:17:38 +0100

    wip: use rmt peripheral

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,22 @@ use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl,
-    embassy::{self,},
-    peripherals::Peripherals,
-    prelude::*,
-    timer::TimerGroup,
+    clock::ClockControl, embassy, gpio::{Gpio6, Gpio7, Output, PushPull, IO}, peripherals::Peripherals, prelude::*, rmt::{asynch::RxChannelAsync, PulseCode, Rmt, RxChannelConfig, RxChannelCreator, TxChannelConfig, TxChannelCreator}, timer::TimerGroup
 };
 
 #[embassy_executor::task]
-async fn run() {
+async fn run(mut rx_channel: esp_hal::rmt::Channel<2>) {
     loop {
-        esp_println::println!("Hello world from embassy using esp-hal-async!");
-        Timer::after(Duration::from_millis(1_000)).await;
+        // what does this do?
+        let mut data = [PulseCode {
+            level1: true,
+            length1: 1,
+            level2: false,
+            length2: 1,
+        }; 48];
+
+        let result = rx_channel.receive(&mut data).await;
+        esp_println::println!("Received: {:?}", data);
     }
 }
 
@@ -39,7 +43,32 @@ async fn main(spawner: Spawner) {
     let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
     embassy::init(&clocks, timg0);
 
-    spawner.spawn(run()).ok();
+    // IO
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let button = io.pins.gpio9.into_pull_up_input();
+
+    // RMT
+    let rmt = Rmt::new(peripherals.RMT, 80u32.MHz(), &clocks).unwrap();
+
+    // IR RECV (GPIO6)
+    let ir_recv = io.pins.gpio6;
+    let rx_config = RxChannelConfig {
+        clk_divider: 255,
+        idle_threshold: 10000,
+        ..RxChannelConfig::default()
+    };
+    let rx_channel = rmt.channel2.configure(ir_recv, rx_config).unwrap();
+
+    // IR TX (GPIO7)
+    let ir_tx_pin = io.pins.gpio7;
+    let tx_config = TxChannelConfig {
+        clk_divider: 255,
+        ..TxChannelConfig::default()
+    };
+    let tx_channel = rmt.channel0.configure(ir_tx_pin, tx_config).unwrap();
+    
+
+    spawner.spawn(run(rx_channel)).ok();
 
     loop {
         esp_println::println!("Bing!");
